e55343cb526192cbdb9906ea68f7c0863553723e
zkxxx <zhangk@servyou.com.cn>
2021-03-09 19:40:36 +0800

    Merge branch 'develop' of C:\Users\zkxxx\Desktop\疫情打卡\dnmd_aliyun with conflicts.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from utils.common import get_conn
 from utils.notice_tools import wechat_notice, Email
 
 if __name__ == '__main__':
+    111111
+    2222222
     conn = get_conn()
     # user_infos = conn.lrange('tt', 0, -1)  # 提取list
     # print(user_infos)
