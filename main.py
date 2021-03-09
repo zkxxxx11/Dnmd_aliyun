d812e7a72ac0a60d11a59b9fdfa58c9f852706b6
zkxxx <zhangk@servyou.com.cn>
2021-03-09 20:14:31 +0800

    <feat>

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from utils.common import get_conn
 from utils.notice_tools import Email
 
 if __name__ == '__main__':
-    aaaaaaaaaaaaaaa
     conn = get_conn()
     # user_infos = conn.lrange('tt', 0, -1)  # 提取list
     # print(user_infos)
